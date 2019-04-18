697c0436d4dfe2d069b21d0e08029c61f875f9f7
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2019-04-18 21:19:13 +0200

    Add static build recipe

--- release.nix
+++ release.nix
@@ -1,3 +1,19 @@
 {
-  hamtsolo = import ./default.nix {};
+  nixpkgs ? <nixpkgs>,
+  pkgs ? import nixpkgs {}
+}:
+
+let
+  stackage = pkgs: import ./stack_overlay.nix { inherit pkgs; };
+  pkgsMusl = pkgs.pkgsMusl;
+in {
+  inherit (stackage pkgs) hamtsolo;
+  hamtsolo-static = (stackage pkgs.pkgsMusl).hamtsolo.overrideAttrs (old: {
+    configureFlags = old.configureFlags ++ [
+      "--ghc-option=-optl=-static"
+      "--ghc-option=-optl=-pthread"
+      "--extra-lib-dirs=${pkgsMusl.gmp6.override { withStatic = true; }}/lib"
+      "--extra-lib-dirs=${pkgsMusl.zlib.static}/lib"
+    ];
+  });
 }
