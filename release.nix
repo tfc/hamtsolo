ec03ab5fdc8c3527fac845a1a91f7f6297947c98
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2019-04-19 08:19:59 +0200

    Add nix support file to provide nice download link in hydra

--- release.nix
+++ release.nix
@@ -15,5 +15,9 @@ in {
       "--extra-lib-dirs=${pkgsMusl.gmp6.override { withStatic = true; }}/lib"
       "--extra-lib-dirs=${pkgsMusl.zlib.static}/lib"
     ];
+    installPhase = old.installPhase + ''
+      mkdir -p $out/nix-support
+      echo "file binary-dist $out/bin/hamtsolo" > $out/nix-support/hydra-build-products
+    '';
   });
 }
