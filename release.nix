dcb02e878ac957aac614842421c90bb119dffe09
Jacek Galowicz <jacek@galowicz.de>
2021-04-06 16:44:58 +0200

    use pinned haskell-nix

--- release.nix
+++ release.nix
@@ -1,23 +1,29 @@
 {
-  nixpkgs ? <nixpkgs>,
-  pkgs ? import nixpkgs {}
+  niv ? import ./nix/sources.nix,
+  nixpkgs ? niv.nixpkgs
 }:
 
 let
-  stackage = pkgs: import ./stack_overlay.nix { inherit pkgs; };
-  pkgsMusl = pkgs.pkgsMusl;
+  haskell-nix = import niv.haskell-nix;
+  pkgs = import nixpkgs { inherit (haskell-nix) overlays; };
+  pkgSet = p: p.haskell-nix.stackProject {
+    src = p.haskell-nix.haskellLib.cleanGit {
+      name = "hamtsolo";
+      src = ./.;
+    };
+  };
 in {
-  inherit (stackage pkgs) hamtsolo;
-  hamtsolo-static = (stackage pkgs.pkgsMusl).hamtsolo.overrideAttrs (old: {
+  hamtsolo = (pkgSet pkgs).hamtsolo.components.all;
+  hamtsolo-static = (pkgSet pkgs.pkgsMusl).hamtsolo.overrideAttrs (old: {
     configureFlags = old.configureFlags ++ [
       "--ghc-option=-optl=-static"
       "--ghc-option=-optl=-pthread"
-      "--extra-lib-dirs=${pkgsMusl.gmp6.override { withStatic = true; }}/lib"
-      "--extra-lib-dirs=${pkgsMusl.zlib.static}/lib"
+      "--extra-lib-dirs=${pkgs.pkgsMusl.gmp6.override { withStatic = true; }}/lib"
+      "--extra-lib-dirs=${pkgs.pkgsMusl.zlib.static}/lib"
     ];
     installPhase = old.installPhase + ''
       mkdir -p $out/nix-support
       echo "file binary-dist $out/bin/hamtsolo" > $out/nix-support/hydra-build-products
     '';
-  });
+  }).hamtsolo.components.all;
 }
