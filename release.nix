6b54de7f0dd7c63cd9d4faa097d0692f7098c0ee
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2022-01-20 16:50:38 +0100

    Add static build variant again

--- release.nix
+++ release.nix
@@ -6,4 +6,19 @@
 
 {
   hamtsolo = pkgs.haskellPackages.callCabal2nix "hamtsolo" ./. {};
+  hamtsolo-static =
+    let
+      pkgsStatic = pkgs.pkgsMusl;
+      inherit (pkgsStatic.haskell.lib) appendConfigureFlags justStaticExecutables;
+      hamtsoloMusl = pkgsStatic.haskellPackages.callCabal2nix "hamtsolo" ./. {};
+    in
+    appendConfigureFlags (justStaticExecutables hamtsoloMusl)
+      [
+        "--disable-shared"
+        "--ghc-option=-optl=-static"
+        "--extra-lib-dirs=${pkgsStatic.gmp6.override { withStatic = true; }}/lib"
+        "--extra-lib-dirs=${pkgsStatic.zlib.static}/lib"
+        "--extra-lib-dirs=${pkgsStatic.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
+        "--extra-lib-dirs=${pkgsStatic.ncurses.override { enableStatic = true; }}/lib"
+      ];
 }
