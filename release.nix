6912ea434254d4817660e5beb92b2595ea5f0ca3
Jacek Galowicz <jacek@galowicz.de>
2022-01-26 18:03:20 +0100

    Rigorously filter src attribute

--- release.nix
+++ release.nix
@@ -1,16 +1,36 @@
-{
-  niv ? import ./nix/sources.nix,
-  nixpkgs ? niv.nixpkgs,
-  pkgs ? import nixpkgs {}
+{ niv ? import ./nix/sources.nix
+, nixpkgs ? niv.nixpkgs
+, pkgs ? import nixpkgs { }
 }:
 
+let
+  sourceByRegex = src: regexes:
+    builtins.filterSource
+      (
+        path: type:
+          let
+            relPath = pkgs.lib.removePrefix "${toString src}/" (toString path);
+            match = builtins.match (pkgs.lib.strings.concatStringsSep "|" regexes);
+          in
+          (type == "directory" && match (relPath + "/") != null || match relPath != null)
+      )
+      src;
+
+  src = sourceByRegex ./. [
+    "src/"
+    "src/.*\.hs"
+    ".*\.cabal"
+    "Setup.hs"
+    "LICENSE"
+  ];
+in
 {
-  hamtsolo = pkgs.haskellPackages.callCabal2nix "hamtsolo" ./. {};
+  hamtsolo = pkgs.haskellPackages.callCabal2nix "hamtsolo" src { };
   hamtsolo-static =
     let
       pkgsStatic = pkgs.pkgsMusl;
       inherit (pkgsStatic.haskell.lib) appendConfigureFlags justStaticExecutables;
-      hamtsoloMusl = pkgsStatic.haskellPackages.callCabal2nix "hamtsolo" ./. {};
+      hamtsoloMusl = pkgsStatic.haskellPackages.callCabal2nix "hamtsolo" src { };
     in
     appendConfigureFlags (justStaticExecutables hamtsoloMusl)
       [
